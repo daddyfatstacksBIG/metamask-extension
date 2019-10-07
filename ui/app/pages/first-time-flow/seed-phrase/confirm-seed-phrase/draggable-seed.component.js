dcea5ff3e3d9487e63027f9c5b72ce35c46fd4ff
Restyled.io <commits@restyled.io>
2019-10-07 18:12:50 +0000

    Restyled by prettier

--- ui/app/pages/first-time-flow/seed-phrase/confirm-seed-phrase/draggable-seed.component.js
+++ ui/app/pages/first-time-flow/seed-phrase/confirm-seed-phrase/draggable-seed.component.js
@@ -1,13 +1,12 @@
-import classnames from 'classnames'
-import PropTypes from 'prop-types'
-import React, {Component} from 'react'
-import {DragSource, DropTarget} from 'react-dnd'
+import classnames from "classnames";
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { DragSource, DropTarget } from "react-dnd";
 
 class DraggableSeed extends Component {
-
   static propTypes = {
     // React DnD Props
-    connectDragSource : PropTypes.func.isRequired,
+    connectDragSource: PropTypes.func.isRequired,
     connectDropTarget: PropTypes.func.isRequired,
     isDragging: PropTypes.bool,
     isOver: PropTypes.bool,
@@ -20,18 +19,18 @@ class DraggableSeed extends Component {
     word: PropTypes.string,
     className: PropTypes.string,
     selected: PropTypes.bool,
-    droppable: PropTypes.bool,
-  }
+    droppable: PropTypes.bool
+  };
 
   static defaultProps = {
-    className : '',
-    onClick() {},
-  }
+    className: "",
+    onClick() {}
+  };
 
   componentWillReceiveProps(nextProps) {
-    const {isOver, setHoveringIndex} = this.props
+    const { isOver, setHoveringIndex } = this.props;
     if (isOver && !nextProps.isOver) {
-      setHoveringIndex(-1)
+      setHoveringIndex(-1);
     }
   }
 
@@ -46,79 +45,88 @@ class DraggableSeed extends Component {
       className,
       onClick,
       isOver,
-      canDrop,
-    } = this.props
+      canDrop
+    } = this.props;
 
-    return connectDropTarget(connectDragSource(
-      <div
-    key = {index} className = {classnames(
-        'btn-secondary notranslate confirm-seed-phrase__seed-word', className, {
-          'confirm-seed-phrase__seed-word--selected btn-primary' : selected,
-          'confirm-seed-phrase__seed-word--dragging': isDragging,
-          'confirm-seed-phrase__seed-word--empty': !word,
-          'confirm-seed-phrase__seed-word--active-drop': !isOver && canDrop,
-          'confirm-seed-phrase__seed-word--drop-hover': isOver && canDrop,
-        })} onClick =
-        {onClick} > {word} <
-        /div>
-    ))
+    return connectDropTarget(
+      connectDragSource(
+        <div
+          key={index}
+          className={classnames(
+            "btn-secondary notranslate confirm-seed-phrase__seed-word",
+            className,
+            {
+              "confirm-seed-phrase__seed-word--selected btn-primary": selected,
+              "confirm-seed-phrase__seed-word--dragging": isDragging,
+              "confirm-seed-phrase__seed-word--empty": !word,
+              "confirm-seed-phrase__seed-word--active-drop": !isOver && canDrop,
+              "confirm-seed-phrase__seed-word--drop-hover": isOver && canDrop
+            }
+          )}
+          onClick={onClick}
+        >
+          {" "}
+          {word}{" "}
+        </div>
+      )
+    );
   }
 }
 
-const SEEDWORD = 'SEEDWORD'
+const SEEDWORD = "SEEDWORD";
 
 const seedSource = {
-  beginDrag (props) {
-    setTimeout(() => props.setDraggingSeedIndex(props.seedIndex), 0)
+  beginDrag(props) {
+    setTimeout(() => props.setDraggingSeedIndex(props.seedIndex), 0);
     return {
       seedIndex: props.seedIndex,
-      word: props.word,
-    }
+      word: props.word
+    };
   },
-  canDrag (props) {
-    return props.draggable
+  canDrag(props) {
+    return props.draggable;
   },
-  endDrag (props, monitor) {
-    const dropTarget = monitor.getDropResult()
+  endDrag(props, monitor) {
+    const dropTarget = monitor.getDropResult();
 
     if (!dropTarget) {
-      setTimeout(() => props.setDraggingSeedIndex(-1), 0)
-      return
+      setTimeout(() => props.setDraggingSeedIndex(-1), 0);
+      return;
     }
 
-    props.onDrop(dropTarget.targetIndex)
-  },
-}
+    props.onDrop(dropTarget.targetIndex);
+  }
+};
 
 const seedTarget = {
-  drop (props) {
+  drop(props) {
     return {
-      targetIndex: props.index,
-    }
+      targetIndex: props.index
+    };
   },
-  canDrop (props) {
-    return props.droppable
+  canDrop(props) {
+    return props.droppable;
   },
-  hover (props) {
-    props.setHoveringIndex(props.index)
-  },
-}
+  hover(props) {
+    props.setHoveringIndex(props.index);
+  }
+};
 
 const collectDrag = (connect, monitor) => {
   return {
     connectDragSource: connect.dragSource(),
-    isDragging: monitor.isDragging(),
-  }
-}
+    isDragging: monitor.isDragging()
+  };
+};
 
 const collectDrop = (connect, monitor) => {
   return {
     connectDropTarget: connect.dropTarget(),
     isOver: monitor.isOver(),
-    canDrop: monitor.canDrop(),
-  }
-}
+    canDrop: monitor.canDrop()
+  };
+};
 
-export default DropTarget(SEEDWORD, seedTarget, collectDrop)(DragSource(SEEDWORD, seedSource, collectDrag)(DraggableSeed))
-
-
+export default DropTarget(SEEDWORD, seedTarget, collectDrop)(
+  DragSource(SEEDWORD, seedSource, collectDrag)(DraggableSeed)
+);
--- ui/app/pages/settings/advanced-tab/advanced-tab.component.js
+++ ui/app/pages/settings/advanced-tab/advanced-tab.component.js
@@ -1,21 +1,21 @@
-import classnames from 'classnames'
-import PropTypes from 'prop-types'
-import React, {PureComponent} from 'react'
+import classnames from "classnames";
+import PropTypes from "prop-types";
+import React, { PureComponent } from "react";
 
-import Button from '../../../components/ui/button'
-import TextField from '../../../components/ui/text-field'
-import ToggleButton from '../../../components/ui/toggle-button'
-import {MOBILE_SYNC_ROUTE} from '../../../helpers/constants/routes'
-import {exportAsFile} from '../../../helpers/utils/util'
+import Button from "../../../components/ui/button";
+import TextField from "../../../components/ui/text-field";
+import ToggleButton from "../../../components/ui/toggle-button";
+import { MOBILE_SYNC_ROUTE } from "../../../helpers/constants/routes";
+import { exportAsFile } from "../../../helpers/utils/util";
 
 export default class AdvancedTab extends PureComponent {
   static contextTypes = {
-    t : PropTypes.func,
-    metricsEvent: PropTypes.func,
-  }
+    t: PropTypes.func,
+    metricsEvent: PropTypes.func
+  };
 
   static propTypes = {
-    setUseNonceField : PropTypes.func,
+    setUseNonceField: PropTypes.func,
     useNonceField: PropTypes.bool,
     setHexDataFeatureFlag: PropTypes.func,
     setRpcTarget: PropTypes.func,
@@ -32,48 +32,48 @@ export default class AdvancedTab extends PureComponent {
     setShowFiatConversionOnTestnetsPreference: PropTypes.func.isRequired,
     threeBoxSyncingAllowed: PropTypes.bool.isRequired,
     setThreeBoxSyncingPermission: PropTypes.func.isRequired,
-    threeBoxDisabled: PropTypes.bool.isRequired,
-  }
+    threeBoxDisabled: PropTypes.bool.isRequired
+  };
 
-  state = {autoLogoutTimeLimit : this.props.autoLogoutTimeLimit}
+  state = { autoLogoutTimeLimit: this.props.autoLogoutTimeLimit };
 
   renderMobileSync() {
-    const {t} = this.context
-    const {history} = this.props
+    const { t } = this.context;
+    const { history } = this.props;
     //
     return (
       <div className="settings-page__content-row">
         <div className="settings-page__content-item">
-          <span>{ t('syncWithMobile') }</span>
+          <span>{t("syncWithMobile")}</span>
         </div>
         <div className="settings-page__content-item">
           <div className="settings-page__content-item-col">
             <Button
-    type = "secondary"
-    large
+              type="secondary"
+              large
               onClick={event => {
-      event.preventDefault()
-      history.push(MOBILE_SYNC_ROUTE)
+                event.preventDefault();
+                history.push(MOBILE_SYNC_ROUTE);
               }}
             >
-              { t('syncWithMobile') }
+              {t("syncWithMobile")}
             </Button>
           </div>
         </div>
       </div>
-    )
+    );
   }
 
   renderStateLogs() {
-    const {t} = this.context
-    const {displayWarning} = this.props
+    const { t } = this.context;
+    const { displayWarning } = this.props;
 
     return (
       <div className="settings-page__content-row">
         <div className="settings-page__content-item">
-          <span>{ t('stateLogs') }</span>
+          <span>{t("stateLogs")}</span>
           <span className="settings-page__content-description">
-            { t('stateLogsDescription') }
+            {t("stateLogsDescription")}
           </span>
         </div>
         <div className="settings-page__content-item">
@@ -84,29 +84,29 @@ export default class AdvancedTab extends PureComponent {
               onClick={() => {
                 window.logStateString((err, result) => {
                   if (err) {
-                    displayWarning(t('stateLogError'))
+                    displayWarning(t("stateLogError"));
                   } else {
-                    exportAsFile(`${t('stateLogFileName')}.json`, result)
+                    exportAsFile(`${t("stateLogFileName")}.json`, result);
                   }
-                })
+                });
               }}
             >
-              { t('downloadStateLogs') }
+              {t("downloadStateLogs")}
             </Button>
           </div>
         </div>
       </div>
-    )
+    );
   }
 
-  renderResetAccount () {
-    const { t } = this.context
-    const { showResetAccountConfirmationModal } = this.props
+  renderResetAccount() {
+    const { t } = this.context;
+    const { showResetAccountConfirmationModal } = this.props;
 
     return (
       <div className="settings-page__content-row">
         <div className="settings-page__content-item">
-          <span>{ t('resetAccount') }</span>
+          <span>{t("resetAccount")}</span>
         </div>
         <div className="settings-page__content-item">
           <div className="settings-page__content-item-col">
@@ -115,35 +115,35 @@ export default class AdvancedTab extends PureComponent {
               large
               className="settings-tab__button--orange"
               onClick={event => {
-                event.preventDefault()
+                event.preventDefault();
                 this.context.metricsEvent({
                   eventOpts: {
-                    category: 'Settings',
-                    action: 'Reset Account',
-                    name: 'Reset Account',
-                  },
-                })
-                showResetAccountConfirmationModal()
+                    category: "Settings",
+                    action: "Reset Account",
+                    name: "Reset Account"
+                  }
+                });
+                showResetAccountConfirmationModal();
               }}
             >
-              { t('resetAccount') }
+              {t("resetAccount")}
             </Button>
           </div>
         </div>
       </div>
-    )
+    );
   }
 
-  renderHexDataOptIn () {
-    const { t } = this.context
-    const { sendHexData, setHexDataFeatureFlag } = this.props
+  renderHexDataOptIn() {
+    const { t } = this.context;
+    const { sendHexData, setHexDataFeatureFlag } = this.props;
 
     return (
       <div className="settings-page__content-row">
         <div className="settings-page__content-item">
-          <span>{ t('showHexData') }</span>
+          <span>{t("showHexData")}</span>
           <div className="settings-page__content-description">
-            { t('showHexDataDescription') }
+            {t("showHexDataDescription")}
           </div>
         </div>
         <div className="settings-page__content-item">
@@ -151,26 +151,25 @@ export default class AdvancedTab extends PureComponent {
             <ToggleButton
               value={sendHexData}
               onToggle={value => setHexDataFeatureFlag(!value)}
-              offLabel={t('off')}
-              onLabel={
-      t('on')}
+              offLabel={t("off")}
+              onLabel={t("on")}
             />
           </div>
         </div>
       </div>
-    )
+    );
   }
 
   renderAdvancedGasInputInline() {
-    const {t} = this.context
-    const {advancedInlineGas, setAdvancedInlineGasFeatureFlag} = this.props
+    const { t } = this.context;
+    const { advancedInlineGas, setAdvancedInlineGasFeatureFlag } = this.props;
 
     return (
       <div className="settings-page__content-row">
         <div className="settings-page__content-item">
-          <span>{ t('showAdvancedGasInline') }</span>
+          <span>{t("showAdvancedGasInline")}</span>
           <div className="settings-page__content-description">
-            { t('showAdvancedGasInlineDescription') }
+            {t("showAdvancedGasInlineDescription")}
           </div>
         </div>
         <div className="settings-page__content-item">
@@ -178,55 +177,56 @@ export default class AdvancedTab extends PureComponent {
             <ToggleButton
               value={advancedInlineGas}
               onToggle={value => setAdvancedInlineGasFeatureFlag(!value)}
-              offLabel={t('off')}
-              onLabel={t('on')}
+              offLabel={t("off")}
+              onLabel={t("on")}
             />
           </div>
         </div>
       </div>
-    )
+    );
   }
 
-  renderShowConversionInTestnets () {
-    const { t } = this.context
+  renderShowConversionInTestnets() {
+    const { t } = this.context;
     const {
       showFiatInTestnets,
-      setShowFiatConversionOnTestnetsPreference,
-    } = this.props
+      setShowFiatConversionOnTestnetsPreference
+    } = this.props;
 
     return (
       <div className="settings-page__content-row">
         <div className="settings-page__content-item">
-          <span>{ t('showFiatConversionInTestnets') }</span>
+          <span>{t("showFiatConversionInTestnets")}</span>
           <div className="settings-page__content-description">
-            { t('showFiatConversionInTestnetsDescription') }
+            {t("showFiatConversionInTestnetsDescription")}
           </div>
         </div>
         <div className="settings-page__content-item">
           <div className="settings-page__content-item-col">
             <ToggleButton
               value={showFiatInTestnets}
-              onToggle={value => setShowFiatConversionOnTestnetsPreference(!value)}
-              offLabel={t('off')}
-              onLabel={
-      t('on')}
+              onToggle={value =>
+                setShowFiatConversionOnTestnetsPreference(!value)
+              }
+              offLabel={t("off")}
+              onLabel={t("on")}
             />
           </div>
         </div>
       </div>
-    )
+    );
   }
 
   renderUseNonceOptIn() {
-    const {t} = this.context
-    const {useNonceField, setUseNonceField} = this.props
+    const { t } = this.context;
+    const { useNonceField, setUseNonceField } = this.props;
 
     return (
       <div className="settings-page__content-row">
         <div className="settings-page__content-item">
-          <span>{ this.context.t('nonceField') }</span>
+          <span>{this.context.t("nonceField")}</span>
           <div className="settings-page__content-description">
-            { t('nonceFieldDescription') }
+            {t("nonceFieldDescription")}
           </div>
         </div>
         <div className="settings-page__content-item">
@@ -234,122 +234,124 @@ export default class AdvancedTab extends PureComponent {
             <ToggleButton
               value={useNonceField}
               onToggle={value => setUseNonceField(!value)}
-              offLabel={t('off')}
-              onLabel={t('on')}
+              offLabel={t("off")}
+              onLabel={t("on")}
             />
           </div>
         </div>
       </div>
-    )
+    );
   }
 
-  renderAutoLogoutTimeLimit () {
-    const { t } = this.context
-    const {
-      autoLogoutTimeLimit,
-      setAutoLogoutTimeLimit,
-    } = this.props
+  renderAutoLogoutTimeLimit() {
+    const { t } = this.context;
+    const { autoLogoutTimeLimit, setAutoLogoutTimeLimit } = this.props;
 
     return (
       <div className="settings-page__content-row">
         <div className="settings-page__content-item">
-          <span>{ t('autoLogoutTimeLimit') }</span>
+          <span>{t("autoLogoutTimeLimit")}</span>
           <div className="settings-page__content-description">
-            { t('autoLogoutTimeLimitDescription') }
+            {t("autoLogoutTimeLimitDescription")}
           </div>
         </div>
         <div className="settings-page__content-item">
           <div className="settings-page__content-item-col">
             <TextField
-    type = "number"
-    id = "autoTimeout"
-    placeholder = "5"
-    value = {this.state.autoLogoutTimeLimit} defaultValue = {
-        autoLogoutTimeLimit} onChange = {e => this.setState({
-      autoLogoutTimeLimit : Math.max(Number(e.target.value), 0)
-    })} fullWidth
-    margin = "dense"
-    min =
-    {
-      0
-    } />
+              type="number"
+              id="autoTimeout"
+              placeholder="5"
+              value={this.state.autoLogoutTimeLimit}
+              defaultValue={autoLogoutTimeLimit}
+              onChange={e =>
+                this.setState({
+                  autoLogoutTimeLimit: Math.max(Number(e.target.value), 0)
+                })
+              }
+              fullWidth
+              margin="dense"
+              min={0}
+            />
             <button
               className="button btn-primary settings-tab__rpc-save-button"
               onClick={() => {
-                setAutoLogoutTimeLimit(this.state.autoLogoutTimeLimit)
+                setAutoLogoutTimeLimit(this.state.autoLogoutTimeLimit);
               }}
             >
-              { t('save') }
-            </button >
+              {t("save")}
+            </button>
+          </div>
         </div>
-        </div>
-        </div>
-    )
+      </div>
+    );
   }
 
-  renderThreeBoxControl () {
-    const { t } = this.context
+  renderThreeBoxControl() {
+    const { t } = this.context;
     const {
       threeBoxSyncingAllowed,
       setThreeBoxSyncingPermission,
-      threeBoxDisabled,
-    } = this.props
+      threeBoxDisabled
+    } = this.props;
 
-    let allowed = threeBoxSyncingAllowed
-    let description = t('syncWithThreeBoxDescription')
+    let allowed = threeBoxSyncingAllowed;
+    let description = t("syncWithThreeBoxDescription");
 
     if (threeBoxDisabled) {
-      allowed = false
-      description = t('syncWithThreeBoxDisabled')
+      allowed = false;
+      description = t("syncWithThreeBoxDisabled");
     }
     return (
       <div className="settings-page__content-row">
         <div className="settings-page__content-item">
-          <span>{ t('syncWithThreeBox') }</span>
-        <div className = "settings-page__content-description">{
-            description}</div>
-        </div>< div
-    className = {classnames('settings-page__content-item', {
-                  'settings-page__content-item--disabled' : threeBoxDisabled,
-                })} > <div className = "settings-page__content-item-col"><
-                ToggleButton
+          <span>{t("syncWithThreeBox")}</span>
+          <div className="settings-page__content-description">
+            {description}
+          </div>
+        </div>
+        <div
+          className={classnames("settings-page__content-item", {
+            "settings-page__content-item--disabled": threeBoxDisabled
+          })}
+        >
+          {" "}
+          <div className="settings-page__content-item-col">
+            <ToggleButton
               value={allowed}
-              onToggle={
-      value => {
-        if (!threeBoxDisabled) {
-          setThreeBoxSyncingPermission(!value)
-        }
-      }}
-              offLabel={t('off')}
-              onLabel={
-      t('on')}
+              onToggle={value => {
+                if (!threeBoxDisabled) {
+                  setThreeBoxSyncingPermission(!value);
+                }
+              }}
+              offLabel={t("off")}
+              onLabel={t("on")}
             />
           </div>
         </div>
       </div>
-    )
+    );
   }
 
   renderContent() {
-    const {warning} = this.props
+    const { warning } = this.props;
 
     return (
       <div className="settings-page__body">
-        { warning && <div className="settings-tab__error">{ warning }</div> }
-        { this.renderStateLogs() }
-        { this.renderMobileSync() }
-        { this.renderResetAccount() }
-        { this.renderAdvancedGasInputInline() }
-        { this.renderHexDataOptIn() }
-        { this.renderShowConversionInTestnets() }
-        { this.renderUseNonceOptIn() }
-        { this.renderAutoLogoutTimeLimit() }
-        { this.renderThreeBoxControl() }
+        {warning && <div className="settings-tab__error">{warning}</div>}
+        {this.renderStateLogs()}
+        {this.renderMobileSync()}
+        {this.renderResetAccount()}
+        {this.renderAdvancedGasInputInline()}
+        {this.renderHexDataOptIn()}
+        {this.renderShowConversionInTestnets()}
+        {this.renderUseNonceOptIn()}
+        {this.renderAutoLogoutTimeLimit()}
+        {this.renderThreeBoxControl()}
       </div>
-    )
+    );
   }
 
-  render () {
-      return this.renderContent()
+  render() {
+    return this.renderContent();
   }
-  }
+}
