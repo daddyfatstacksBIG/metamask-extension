ed2ce019a467632353068332d1b436a70fc7c64e
Restyled.io <commits@restyled.io>
2019-11-22 23:44:13 +0000

    Restyled by clang-format

--- ui/app/components/app/dropdowns/components/dropdown.js
+++ ui/app/components/app/dropdowns/components/dropdown.js
@@ -5,7 +5,7 @@ const MenuDroppo = require('../../menu-droppo')
 const extend = require('xtend')
 
 class Dropdown extends Component {
-  render () {
+  render() {
     const {
       containerClassName,
       isOpen,
@@ -17,91 +17,84 @@ class Dropdown extends Component {
     } = this.props
 
     const innerStyleDefaults = extend({
-      borderRadius: '4px',
-      padding: '8px 16px',
-      background: 'rgba(0, 0, 0, 0.8)',
-      boxShadow: 'rgba(0, 0, 0, 0.15) 0px 2px 2px 2px',
-    }, innerStyle)
+      borderRadius : '4px',
+      padding : '8px 16px',
+      background : 'rgba(0, 0, 0, 0.8)',
+      boxShadow : 'rgba(0, 0, 0, 0.15) 0px 2px 2px 2px',
+    },
+                                      innerStyle)
 
-    return h(
-      MenuDroppo,
-      {
-        containerClassName,
-        useCssTransition,
-        isOpen,
-        zIndex: 55,
-        onClickOutside,
-        style,
-        innerStyle: innerStyleDefaults,
-      },
-      [
-        h(
-          'style',
-          `
+    return h(MenuDroppo, {
+      containerClassName,
+      useCssTransition,
+      isOpen,
+      zIndex : 55,
+      onClickOutside,
+      style,
+      innerStyle : innerStyleDefaults,
+    },
+             [
+               h('style', `
           li.dropdown-menu-item:hover {
             color:rgb(225, 225, 225);
             background-color: rgba(255, 255, 255, 0.05);
             border-radius: 4px;
           }
           li.dropdown-menu-item { color: rgb(185, 185, 185); }
-          `
-        ),
-        ...children,
-      ]
-    )
+          `),
+               ...children,
+             ])
   }
 }
 
 Dropdown.defaultProps = {
-  useCssTransition: false,
+  useCssTransition : false,
 }
 
-Dropdown.propTypes = {
-  isOpen: PropTypes.bool.isRequired,
-  children: PropTypes.node,
-  style: PropTypes.object.isRequired,
-  onClickOutside: PropTypes.func,
-  innerStyle: PropTypes.object,
-  useCssTransition: PropTypes.bool,
-  containerClassName: PropTypes.string,
+                        Dropdown.propTypes = {
+  isOpen : PropTypes.bool.isRequired,
+  children : PropTypes.node,
+  style : PropTypes.object.isRequired,
+  onClickOutside : PropTypes.func,
+  innerStyle : PropTypes.object,
+  useCssTransition : PropTypes.bool,
+  containerClassName : PropTypes.string,
 }
 
 class DropdownMenuItem extends Component {
-  render () {
-    const { onClick, closeMenu, children, style } = this.props
+  render() {
+    const {onClick, closeMenu, children, style} = this.props
 
-    return h(
-      'li.dropdown-menu-item',
-      {
-        onClick: () => {
-          onClick()
-          closeMenu()
-        },
-        style: Object.assign({
-          listStyle: 'none',
-          padding: '8px 0px',
-          fontSize: '18px',
-          fontStyle: 'normal',
-          cursor: 'pointer',
-          display: 'flex',
-          justifyContent: 'flex-start',
-          alignItems: 'center',
-          color: 'white',
-        }, style),
+    return h('li.dropdown-menu-item', {
+      onClick : () => {
+        onClick()
+        closeMenu()
       },
-      children
-    )
+      style : Object.assign({
+        listStyle : 'none',
+        padding : '8px 0px',
+        fontSize : '18px',
+        fontStyle : 'normal',
+        cursor : 'pointer',
+        display : 'flex',
+        justifyContent : 'flex-start',
+        alignItems : 'center',
+        color : 'white',
+      },
+                            style),
+    },
+             children)
   }
 }
 
 DropdownMenuItem.propTypes = {
-  closeMenu: PropTypes.func.isRequired,
-  onClick: PropTypes.func.isRequired,
-  children: PropTypes.node,
-  style: PropTypes.object,
+  closeMenu : PropTypes.func.isRequired,
+  onClick : PropTypes.func.isRequired,
+  children : PropTypes.node,
+  style : PropTypes.object,
 }
 
-module.exports = {
+                             module.exports = {
   Dropdown,
   DropdownMenuItem,
 }
--- ui/app/components/app/input-number.js
+++ ui/app/components/app/input-number.js
@@ -11,48 +11,51 @@ const {
 module.exports = InputNumber
 
 inherits(InputNumber, Component)
-function InputNumber () {
-  Component.call(this)
+function InputNumber() {
+  Component
+      .call(this)
 
-  this.setValue = this.setValue.bind(this)
+          this.setValue = this.setValue.bind(this)
 }
 
-function isValidInput (text) {
+function isValidInput(text) {
   const re = /^([1-9]\d*|0)(\.|\.\d*)?$/
   return re.test(text)
 }
 
-function removeLeadingZeroes (str) {
-  return str.replace(/^0*(?=\d)/, '')
-}
+function removeLeadingZeroes(str) { return str.replace(/^0*(?=\d)/, '') }
 
-InputNumber.prototype.setValue = function (newValue) {
+InputNumber.prototype.setValue =
+    function(newValue) {
   newValue = removeLeadingZeroes(newValue)
   if (newValue && !isValidInput(newValue)) {
     return
   }
-  const { fixed, min = -1, max = Infinity, onChange } = this.props
+  const {fixed, min = -1, max = Infinity, onChange} = this.props
 
   newValue = fixed ? newValue.toFixed(4) : newValue
   const newValueGreaterThanMin = conversionGTE(
-    { value: newValue || '0', fromNumericBase: 'dec' },
-    { value: min, fromNumericBase: 'hex' },
+      {value : newValue || '0', fromNumericBase : 'dec'},
+      {value : min, fromNumericBase : 'hex'},
   )
 
   const newValueLessThanMax = conversionLTE(
-    { value: newValue || '0', fromNumericBase: 'dec' },
-    { value: max, fromNumericBase: 'hex' },
+      {value : newValue || '0', fromNumericBase : 'dec'},
+      {value : max, fromNumericBase : 'hex'},
   )
   if (newValueGreaterThanMin && newValueLessThanMax) {
     onChange(newValue)
-  } else if (!newValueGreaterThanMin) {
+  }
+  else if (!newValueGreaterThanMin) {
     onChange(min)
-  } else if (!newValueLessThanMax) {
+  }
+  else if (!newValueLessThanMax) {
     onChange(max)
   }
 }
 
-InputNumber.prototype.render = function InputNumber () {
+    InputNumber.prototype.render =
+        function InputNumber() {
   const { unitLabel, step = 1, placeholder, value } = this.props
 
   return (
